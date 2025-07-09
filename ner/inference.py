dcfb11a0cfc8dc2c0bf87ea07fb28ef22e180462
Nguyen Van Trong <trongnv130@ghtk.co>
2025-07-09 15:48:20 +0700

    Refactor: refactor code inference

--- ner/inference.py
+++ ner/inference.py
@@ -1,8 +1,8 @@
 import json
 import torch
-import argparse
 from typing import List
-from transformers import AutoModelForTokenClassification
+from loguru import logger
+from transformers import AutoModelForTokenClassification, AutoTokenizer, AutoConfig
 
 from ner.services.preprocessing import TextPreprocess
 from ner.phobert_tokenizer_fast.tokenization_phobert_fast import PhobertTokenizerFast
@@ -13,48 +13,25 @@ class Inference:
         self,
         model: torch.nn.Module,
         tokenizer: PhobertTokenizerFast,
-        labels_mapping: dict,
-        device: torch.device = None,
+        id2label: dict,
+        device: torch.device,
         
     ) -> None:
-        """
-        Args:
-            model (torch.nn.Module): Mô hình đã huấn luyện.
-            tokenizer (PhobertTokenizerFast): Tokenizer tương ứng với mô hình.
-            labels_mapping (dict): Từ điển ánh xạ từ chỉ số nhãn sang tên nhãn, ví dụ {0: "O", 1: "B-PER", ...}.
-            device (torch.device, optional): Thiết bị để chạy mô hình (CPU hoặc GPU). Nếu không cung cấp, tự động chọn.
-        """
-
         self.tokenizer = tokenizer
-        self.labels_mapping = labels_mapping
+        self.id2label = id2label
+        self.device = device
         self.process = TextPreprocess()
 
-        if device:
-            self.device = device
-        else:
-            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
         self.model.eval()
 
     def run(self, texts: List[str], max_length: int = 256, output_file: str = None) -> List[dict]:
-        """
-        Infer trên một hoặc nhiều context được cung cấp
-
-        Args:
-            texts (str): Danh sách các văn bản cần kiểm thử.
-            max_length (int, optional): Độ dài tối đa sau khi tokenize. Mặc định là 128.
-            output_file (str, optional): Đường dẫn file để lưu kết quả. Nếu không cung cấp, không lưu.
-
-        Returns:
-            list: Danh sách các dự đoán cho mỗi văn bản.
-        """
-
         all_predictions = []
 
         for text in texts:
             processed_text = self.process.process_text(text)
 
-            tokenized_inputs = self.tokenizer(
+            tensors = self.tokenizer(
                 processed_text,
                 is_split_into_words=True,
                 max_length=max_length,
@@ -62,17 +39,14 @@ class Inference:
                 truncation=True,
                 return_tensors="pt",
             )
-
-            input_ids = tokenized_inputs["input_ids"].to(self.device)
-            attention_mask = tokenized_inputs["attention_mask"].to(self.device)
-            word_ids = tokenized_inputs.word_ids(batch_index=0)
+            word_ids = tensors.word_ids(batch_index=0)
+            tensors = {key: value.to(self.device) for key, value in tensors.items()}
             with torch.no_grad():
-                outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+                outputs = self.model(**tensors)
                 logits = outputs.logits
 
             predictions = torch.argmax(logits, dim=-1).cpu().numpy()[0]
             predicted_label_names = self._map_labels(predictions)
-            # tokens = self.tokenizer.convert_ids_to_tokens(input_ids[0])
             word_labels = []
 
             for idx, word_id in enumerate(word_ids):
@@ -94,94 +68,45 @@ class Inference:
         return all_predictions
 
     def _map_labels(self, label_indices: list) -> list:
-        return [self.labels_mapping.get(idx, "O") for idx in label_indices]
+        return [self.id2label.get(idx, "O") for idx in label_indices]
+
+def get_tokenizer(checkpoint: str):
+    config = AutoConfig.from_pretrained(checkpoint)
+    if "RobertaForTokenClassification" in config.architectures:
+        tokenizer = PhobertTokenizerFast.from_pretrained(checkpoint, use_fast=True)
+        logger.info(f"Using PhobertTokenizerFast for {checkpoint}")
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(checkpoint, add_prefix_space=True, use_fast=True)
+        logger.info(f"Using AutoTokenizer for {checkpoint}")
+    return tokenizer
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="vinai/phobert-base-v2", required=True)
-    parser.add_argument("--output_dir", type=str, default="./models", required=True)
-    parser.add_argument("--input", type=str, required=True)
-    parser.add_argument("--record_output_file", type=str, default="output_infer.json")
-    args = parser.parse_args()
-
-    model_name = args.model.split('/')[-1]
-    save_dir = f"{args.output_dir}/{model_name}"
-
-    tokenizer = PhobertTokenizerFast.from_pretrained(save_dir)
-    model = AutoModelForTokenClassification.from_pretrained(save_dir)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+
+    model_path = "nlp_trainer/ner/models/phobert-base-v2/best_model"
+    output_path = "output_infer.json"
+
+    tokenizer = get_tokenizer(model_path)
+    model = AutoModelForTokenClassification.from_pretrained(model_path)
     model.eval()
 
     text = "Xin chào các bạn, tôi là Trọng, tôi năm nay 21 tuổi. Tôi đến từ Hà Nội, mảnh đất của Việt Nam. Hôm nay tôi đi khám ở bệnh viện Bạch Mai. Tôi vào thăm bệnh nhân bị COVID-19 chiều ngày 15 tháng 9"
 
     infer = Inference(model, tokenizer=tokenizer, id2label=model.config.id2label, device=device)
-
+    max_length = min(model.config.max_position_embeddings, tokenizer.model_max_length)
+    print(f"Max length: {max_length}")
+    
     predictions = infer.run(
-        [args.input],
-        max_length=256,
-        output_file=args.record_output_file
+        [text],
+        max_length=max_length,
+        output_file=output_path,
     )
 
     for prediction in predictions:
         output_text = prediction['text']
         tokens = [token['token'] for token in prediction['tokens']]
         labels = [token['label'] for token in prediction['tokens']]
-
         print("Text:", output_text)
         print("Tokens:", tokens)
         print("Labels:", labels)
-
-
-"""OUTPUT:
-{'text': 'Xin chào các bạn, tôi là Trọng, tôi năm nay 21 tuổi. Tôi đến từ Hà '
-        'Nội, mảnh đất của Việt Nam. Hôm nay tôi đi khám ở bệnh viện Bạch '
-        'Mai. Tôi vào thăm bệnh nhân bị COVID-19 chiều ngày 15 tháng 9',
-'tokens': [{'label': 'O', 'token': 'Xin'},
-        {'label': 'O', 'token': 'chào'},
-        {'label': 'O', 'token': 'các'},
-        {'label': 'O', 'token': 'bạn'},
-        {'label': 'O', 'token': ','},
-        {'label': 'O', 'token': 'tôi'},
-        {'label': 'O', 'token': 'là'},
-        {'label': 'B-NAME', 'token': 'Trọng'},
-        {'label': 'O', 'token': ','},
-        {'label': 'O', 'token': 'tôi'},
-        {'label': 'O', 'token': 'năm'},
-        {'label': 'O', 'token': 'nay'},
-        {'label': 'B-AGE', 'token': '21'},
-        {'label': 'O', 'token': 'tuổi'},
-        {'label': 'O', 'token': '.'},
-        {'label': 'O', 'token': 'Tôi'},
-        {'label': 'O', 'token': 'đến'},
-        {'label': 'O', 'token': 'từ'},
-        {'label': 'B-LOCATION', 'token': 'Hà_Nội'},
-        {'label': 'O', 'token': ','},
-        {'label': 'O', 'token': 'mảnh'},
-        {'label': 'O', 'token': 'đất'},
-        {'label': 'O', 'token': 'của'},
-        {'label': 'B-LOCATION', 'token': 'Việt_Nam'},
-        {'label': 'O', 'token': '.'},
-        {'label': 'O', 'token': 'Hôm_nay'},
-        {'label': 'O', 'token': 'tôi'},
-        {'label': 'O', 'token': 'đi'},
-        {'label': 'O', 'token': 'khám'},
-        {'label': 'O', 'token': 'ở'},
-        {'label': 'B-LOCATION', 'token': 'bệnh_viện'},
-        {'label': 'I-LOCATION', 'token': 'Bạch_Mai'},
-        {'label': 'O', 'token': '.'},
-        {'label': 'O', 'token': 'Tôi'},
-        {'label': 'O', 'token': 'vào'},
-        {'label': 'O', 'token': 'thăm'},
-        {'label': 'O', 'token': 'bệnh_nhân'},
-        {'label': 'O', 'token': 'bị'},
-        {'label': 'O', 'token': 'COVID-19'},
-        {'label': 'O', 'token': 'COVID-19'},
-        {'label': 'O', 'token': 'COVID-19'},
-        {'label': 'O', 'token': 'COVID-19'},
-        {'label': 'O', 'token': 'chiều'},
-        {'label': 'O', 'token': 'ngày'},
-        {'label': 'B-DATE', 'token': '15'},
-        {'label': 'I-DATE', 'token': 'tháng'},
-        {'label': 'I-DATE', 'token': '9'}]}
-"""
